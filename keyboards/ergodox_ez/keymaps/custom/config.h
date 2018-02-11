85e336194450a024246fc153e09a7153f9c927d7
Andrew Kvalheim <Andrew@Kvalhe.im>
2018-02-11 12:10:50 -0800

    Use mod tap on home row, balance thumbs, simplify peripheral keys.

---
+++ keyboards/ergodox_ez/keymaps/custom/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+#include "../../config.h"
+
+// Favor fast tapping over fast modifying.
+#undef PERMISSIVE_HOLD
+
+// Hold the modifier, not tapped key when pressed in quick succession.
+#define TAPPING_FORCE_HOLD
+
+#endif
--- keyboards/ergodox_ez/keymaps/custom/keymap.c
+++ keyboards/ergodox_ez/keymaps/custom/keymap.c
@@ -53,24 +53,24 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BAS] = LAYOUT_ergodox(
     // Left base
-    ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,
-     KC_ESC,    CO_Q,    CO_W,    CO_F,    CO_P,    CO_G, KC_HOME,
-    KC_BSPC,    CO_A,    CO_R,    CO_S,    CO_T,    CO_D,
-    COMPOSE,    CO_Z,    CO_X,    CO_C,    CO_V,    CO_B,  KC_END,
-    ___X___, ___X___, KC_LGUI, KC_LALT, KC_LCTL,
-                                                                   ___X___, KC_PSCR,
-                                                                            ___X___,
-                                                          KC_LSFT, MO(SYM),   ALTGR,
+    ___X___,     ___X___,     ___X___,       ___X___,     ___X___, ___X___, ___X___,
+    COMPOSE,        CO_Q,        CO_W,          CO_F,        CO_P,    CO_G, KC_HOME,
+    KC_BSPC, ALT_T(CO_A), CTL_T(CO_R), LT(SYM, CO_S), SFT_T(CO_T),    CO_D,
+    ___X___,        CO_Z,        CO_X,          CO_C,        CO_V,    CO_B,  KC_END,
+    ___X___,     ___X___,     ___X___,         ALTGR,     KC_LGUI,
+                                                                                     ___X___, KC_PSCR,
+                                                                                               KC_TAB,
+                                                                             KC_ENT,  KC_ESC,  KC_SPC,
 
     // Right base
-                      ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,
-                      KC_PGUP,    CO_J,    CO_L,    CO_U,    CO_Y, CO_SCLN,  KC_DEL,
-                                  CO_H,    CO_N,    CO_E,    CO_I,    CO_O,  KC_TAB,
-                      KC_PGDN,    CO_K,    CO_M, CO_COMM,  CO_DOT, CO_SLSH, KC_RSFT,
-                                        MO(NAV), MO(NUM), MO(FUN), ___X___, ___X___,
+                      ___X___, ___X___,     ___X___,       ___X___,     ___X___,     ___X___, ___X___,
+                      KC_PGUP,    CO_J,        CO_L,          CO_U,        CO_Y,     CO_SCLN, ___X___,
+                                  CO_H, SFT_T(CO_N), LT(SYM, CO_E), CTL_T(CO_I), ALT_T(CO_O),  KC_DEL,
+                      KC_PGDN,    CO_K,        CO_M,       CO_COMM,      CO_DOT,     CO_SLSH, ___X___,
+                                            MO(NAV),       MO(NUM),     ___X___,     ___X___, ___X___,
     KC_RCTL, ___X___,
      KC_ESC,
-    KC_BSPC,  KC_ENT, KC_SPC
+     KC_ENT, KC_TAB, KC_SPC
   ),
   [SYM] = LAYOUT_ergodox(
     // Left symbol
@@ -96,9 +96,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [NUM] = LAYOUT_ergodox(
     // Left number
     _______, _______, _______, _______, _______, _______, _______,
-    _______, KC_PERC, KC_PLUS, KC_ASTR, KC_AMPR, ___X___, _______,
+    MO(FUN), KC_PERC, KC_PLUS, KC_ASTR, KC_AMPR, ___X___, _______,
     _______,  KC_DLR,  KC_EQL, KC_UNDS, KC_MINS, KC_TILD,
-    CTLSFTU, KC_LPRN, KC_RPRN, KC_HASH, KC_PIPE, ___X___, _______,
+    _______, KC_LPRN, KC_RPRN, KC_HASH, KC_PIPE, ___X___, _______,
     _______, _______, _______, _______, _______,
                                                                    _______, _______,
                                                                             _______,
@@ -106,8 +106,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Right number
                       _______, _______, _______, _______, _______, _______, _______,
-                      _______, ___X___,    KC_7,    KC_8,    KC_9, CO_COMM, _______,
-                                  KC_0,    KC_4,    KC_5,    KC_6,  CO_DOT, _______,
+                      _______, ___X___,    KC_7,    KC_8,    KC_9, CO_COMM, CTLSFTU,
+                                  KC_0,    KC_4,    KC_5,    KC_6,  CO_DOT, KC_BSPC,
                       _______, ___X___,    KC_1,    KC_2,    KC_3, CO_SLSH, _______,
                                         _______, _______, _______, _______, _______,
     _______, _______,
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [FUN] = LAYOUT_ergodox(
     // Left function
     _______, _______, _______, _______, _______, _______, _______,
-    TG(HDW), ___X___, ___X___, ___X___, ___X___, ___X___, _______,
+    _______, ___X___, ___X___, ___X___, ___X___, ___X___, _______,
     _______, ___X___, ___X___, ___X___, ___X___, ___X___,
     _______, ___X___, ___X___, ___X___, ___X___, ___X___, _______,
     _______, _______, _______, _______, _______,
@@ -127,7 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Right function
                       _______, _______, _______, _______, _______, _______, _______,
-                      _______, ___X___,   KC_F7,   KC_F8,   KC_F9,  KC_F12, _______,
+                      _______, ___X___,   KC_F7,   KC_F8,   KC_F9,  KC_F12, TG(HDW),
                                ___X___,   KC_F4,   KC_F5,   KC_F6,  KC_F11, _______,
                       _______, ___X___,   KC_F1,   KC_F2,   KC_F3,  KC_F10, _______,
                                         _______, _______, _______, _______, _______,
@@ -144,7 +144,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______,
                                                                    _______, _______,
                                                                             _______,
-                                                          _______,  KC_SPC, _______,
+                                                          _______, _______, _______,
 
     // Right navigation
                       _______, _______, _______, _______, _______, _______, _______,
@@ -158,21 +158,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [HDW] = LAYOUT_ergodox(
     // Left hardware Colemak
-    _______, _______, _______, _______, _______, _______, _______,
-    _______,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G, _______,
-    _______,    KC_A,    KC_R,    KC_S,    KC_T,    KC_D,
-    _______,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, _______,
-    _______, _______, _______, _______, _______,
-                                                                   _______, _______,
-                                                                            _______,
-                                                          _______, _______, _______,
+    _______,     _______,     _______,       _______,     _______, _______, _______,
+    _______,        KC_Q,        KC_W,          KC_F,        KC_P,    KC_G, _______,
+    _______, ALT_T(KC_A), CTL_T(KC_R), LT(SYM, KC_S), SFT_T(KC_T),    KC_D,
+    _______,        KC_Z,        KC_X,          KC_C,        KC_V,    KC_B, _______,
+    _______,     _______,     _______,       _______,     _______,
+                                                                                     _______, _______,
+                                                                                              _______,
+                                                                            _______, _______, _______,
 
     // Right hardware Colemak
-                      _______, _______, _______, _______, _______, _______, _______,
-                      _______,    KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, _______,
-                                  KC_H,    KC_N,    KC_E,    KC_I,    KC_O, _______,
-                      _______,    KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, _______,
-                                        _______, _______, _______, _______, _______,
+                      _______, _______,     _______,       _______,     _______,     _______, _______,
+                      _______,    KC_J,        KC_L,          KC_U,        KC_Y,     KC_SCLN, _______,
+                                  KC_H, SFT_T(KC_N), LT(SYM, KC_E), CTL_T(KC_I), ALT_T(KC_O), _______,
+                      _______,    KC_K,        KC_M,       KC_COMM,      KC_DOT,     KC_SLSH, _______,
+                                            _______,       _______,     _______,     _______, _______,
     TG(HDW), _______,
     _______,
     _______, _______, _______
