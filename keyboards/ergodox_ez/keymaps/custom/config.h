cb3d4c91b4ff51b4a4bf09b9f3c5771c9be85e6b
Andrew Kvalheim <Andrew@Kvalhe.im>
2018-02-12 18:31:30 -0800

    Shorten the tapping term.

--- keyboards/ergodox_ez/keymaps/custom/config.h
+++ keyboards/ergodox_ez/keymaps/custom/config.h
@@ -8,4 +8,8 @@
 // Hold the modifier, not tapped key when pressed in quick succession.
 #define TAPPING_FORCE_HOLD
 
+// Transition from tapping to modifying sooner.
+#undef TAPPING_TERM
+#define TAPPING_TERM 150
+
 #endif
